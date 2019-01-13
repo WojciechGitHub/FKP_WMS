cec85dcd9bb051b115d86939d633417a73acc879
WojciechGitHub <kossowski.wojciech@gmail.com>
2019-01-13 11:47:43 +0100

    first commit - application backbone

---
+++ src/main/java/pl/fkpsystem/FKP_WMS/FkpWmsApplication.java
@@ -0,0 +1,14 @@
+package pl.fkpsystem.FKP_WMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FkpWmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FkpWmsApplication.class, args);
+	}
+
+}
+
